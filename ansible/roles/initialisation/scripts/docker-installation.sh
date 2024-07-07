a8ada83ce76ac5351e570a585c707b40c2ceedf9
m-mattia-m <mattia@mattiamueggler.ch>
2024-07-07 12:26:30 +0200

    fix docker installation

--- ansible/roles/initialisation/scripts/docker-installation.sh
+++ ansible/roles/initialisation/scripts/docker-installation.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 
-# Update the apt package index and install packages to allow apt to use a repository over HTTPS
+SSH_USER="${SUDO_USER:-${USER}}"
+echo "current user: $SSH_USER"
+
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add Docker’s official GPG key
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-# Set up the Docker repository
+# Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Update the apt package index again
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-# Install the latest version of Docker Engine, containerd, and Docker Compose
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-# Add the current user to the docker group
+sudo usermod -a -G docker $SSH_USER
 sudo usermod -aG docker $USER
 
-# Enable Docker to start on boot
-sudo systemctl enable docker
-
-# Print Docker version to verify the installation
-docker --version
-
-# Print a message to indicate that the installation is complete
-echo "Docker installation complete. Please reboot your system for the group changes to take effect."
-
-# Optionally, you can reboot the system automatically (uncomment the following line if desired)
-# sudo reboot
+# Ensure Docker socket permissions for the current session:
+sudo chmod 666 /var/run/docker.sock
